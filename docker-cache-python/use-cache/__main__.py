9208a565ef58f8f1ecf2701cf7c026d53960fc5c
Piers Karsenbarg <piers@pulumi.com>
2021-04-07 13:51:13 +0100

    Updated code to push to latest tag

--- docker-cache-python/use-cache/__main__.py
+++ docker-cache-python/use-cache/__main__.py
@@ -9,7 +9,7 @@ cache_from=CacheFrom()
 cache_from.stages = ["builder"]
 
 runtime_image = Image("runtime_image",
-                    image_name=f'pierskarsenbarg/dockercache-python:builder',
+                    image_name=f'pierskarsenbarg/dockercache-python:latest',
                     build=DockerBuild(target=f'runtime', 
                                     context=f'./app',
                                     cache_from=cache_from))
