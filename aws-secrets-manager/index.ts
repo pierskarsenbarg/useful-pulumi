d27960b13fa98aabde560752d92ded02ad2abd7b
Piers Karsenbarg <piers@pulumi.com>
2021-07-13 14:45:34 +0100

    Renamed pulumi object

--- aws-secrets-manager/index.ts
+++ aws-secrets-manager/index.ts
@@ -2,7 +2,7 @@ import * as aws from "@pulumi/aws";
 
 const secretContainer = new aws.secretsmanager.Secret("secretContainer");
 
-const secret2 = new aws.secretsmanager.SecretVersion("secret2", {
+const secret = new aws.secretsmanager.SecretVersion("secret", {
     secretId: secretContainer.id,
     secretString: "mysecret"
 })
