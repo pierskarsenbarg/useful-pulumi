638d2031e143ea3aa06f037e1f973989f71d3f12
Piers Karsenbarg <piers@pulumi.com>
2022-03-23 10:41:45 +0000

    deleted secrets demo

--- aws-secrets-manager/index.ts
+++
@@ -1,8 +0,0 @@
-import * as aws from "@pulumi/aws";
-
-const secretContainer = new aws.secretsmanager.Secret("secretContainer");
-
-const secret = new aws.secretsmanager.SecretVersion("secret", {
-    secretId: secretContainer.id,
-    secretString: "mysecret"
-})
