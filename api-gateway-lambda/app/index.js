7215ba2821f2588d1fba5c63243056a7a1d73c14
Piers Karsenbarg <piers@pulumi.com>
2023-05-17 20:38:53 +0100

    added apigateway to lambda integration example

---
+++ api-gateway-lambda/app/index.js
@@ -0,0 +1,6 @@
+exports.handler = async (event, context) => {
+    console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+    return {
+        "message": "Hello World!"
+    }
+  };
---
+++ api-gateway-lambda/index.ts
@@ -0,0 +1,76 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+
+const role = new aws.iam.Role("lambda-role", {
+  assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal(
+    aws.iam.Principals.LambdaPrincipal
+  ),
+  managedPolicyArns: [aws.iam.ManagedPolicies.AWSLambdaBasicExecutionRole],
+});
+
+const lambda = new aws.lambda.Function("api-lambda", {
+  code: new pulumi.asset.AssetArchive({
+    ".": new pulumi.asset.FileArchive("./app"),
+  }),
+  role: role.arn,
+  runtime: aws.lambda.Runtime.NodeJS18dX,
+  handler: "index.handler",
+});
+
+const api = new aws.apigateway.RestApi("api");
+
+const method = new aws.apigateway.Method("method", {
+  resourceId: api.rootResourceId,
+  restApi: api.id,
+  httpMethod: "POST",
+  authorization: "NONE",
+});
+
+const integration = new aws.apigateway.Integration("integration", {
+  httpMethod: method.httpMethod,
+  resourceId: api.rootResourceId,
+  restApi: api.id,
+  integrationHttpMethod: "POST",
+  uri: lambda.invokeArn,
+  type: "AWS",
+});
+
+const integrationresponse = new aws.apigateway.IntegrationResponse(
+  "integrationresponse",
+  {
+    httpMethod: method.httpMethod,
+    restApi: api.id,
+    resourceId: api.rootResourceId,
+    statusCode: "200",
+  },
+  {dependsOn: [integration]}
+);
+
+const methodResponse = new aws.apigateway.MethodResponse("methodresponse", {
+  httpMethod: method.httpMethod,
+  resourceId: api.rootResourceId,
+  restApi: api.id,
+  statusCode: "200",
+});
+
+const permission = new aws.lambda.Permission("permission", {
+  action: "lambda:InvokeFunction",
+  function: lambda.name,
+  principal: "apigateway.amazonaws.com",
+  sourceArn: pulumi.interpolate`${api.executionArn}/*/*`,
+});
+
+const deployment = new aws.apigateway.Deployment("deployment", {
+  restApi: api.id,
+}, {
+    // These are all required otherwise the deployment will fail
+    dependsOn: [integration, integrationresponse, method, methodResponse]
+});
+
+const stage = new aws.apigateway.Stage("stage", {
+  deployment: deployment.id,
+  restApi: api.id,
+  stageName: "dev",
+});
+
+export const url = pulumi.interpolate`${stage.invokeUrl}/`;
