bf679b9aba42ec6778fe7b46ec20b4d93cc52388
MitchellGerdisch <mitch@pulumi.com>
2024-05-09 11:19:21 -0500

    Some package updates

--- serverless-webpack/index.ts
+++ serverless-webpack/index.ts
@@ -1,6 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 
 const role = new aws.iam.Role("lambda-demo-role", {
     assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal(aws.iam.Principals.LambdaPrincipal),
